52092d288beea911bd7412f04851b927d8b1d908
Adriano <adriano.bezerra4@fatec.sp.gov.br>
2023-10-02 13:52:53 -0300

    Atualização Modificado const para let

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const titulo = document.getElementById("titulo")
-const ulElement = document.querySelector("ul")
-const aElement = document.querySelector("a")
-const olElement = document.getElementById("lista-ordenada")
+let titulo = document.getElementById("titulo")
+let ulElement = document.querySelector("ul")
+let aElement = document.querySelector("a")
+let olElement = document.getElementById("lista-ordenada")
 
 titulo.innerText = "Título da Página"
 aElement.innerText = "Visite o site da Prozeducação"
@@ -10,7 +10,7 @@ ulElement.innerHTML = `
     <li>Item 1</li>
     <li>Item 2</li>
     <li>Item 3</li>
-`;
+`
 
 olElement.innerHTML = `
     <li><a href="https://exemplo1.com">Link 1</a></li>
