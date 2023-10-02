c059a79f13cf3e90ee1057cf630eaae26f79606a
Adriano <adriano.bezerra4@fatec.sp.gov.br>
2023-10-02 13:27:00 -0300

    Atividade JavaScript I 06

---
+++ script.js
@@ -0,0 +1,19 @@
+const titulo = document.getElementById("titulo")
+const ulElement = document.querySelector("ul")
+const aElement = document.querySelector("a")
+const olElement = document.getElementById("lista-ordenada")
+
+titulo.innerText = "Título da Página"
+aElement.innerText = "Visite o site da Prozeducação"
+
+ulElement.innerHTML = `
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+`;
+
+olElement.innerHTML = `
+    <li><a href="https://exemplo1.com">Link 1</a></li>
+    <li><a href="https://exemplo2.com">Link 2</a></li>
+    <li><a href="https://exemplo3.com">Link 3</a></li>
+`
